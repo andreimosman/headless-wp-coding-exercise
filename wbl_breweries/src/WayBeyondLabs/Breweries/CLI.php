042b02dfa2376b71ca2943f1817bd9ead628a74d
Andrei de Oliveira Mosman <andrei.mosman@gmail.com>
2022-04-01 09:23:39 -0300

    importer cli tool

--- wbl_breweries/src/WayBeyondLabs/Breweries/CLI.php
+++ wbl_breweries/src/WayBeyondLabs/Breweries/CLI.php
@@ -4,7 +4,6 @@ namespace WayBeyondLabs\Breweries;
 
 class CLI {
 
-
     /**
 	 * Import data from the Open Brewery API
 	 *
@@ -13,6 +12,45 @@ class CLI {
 	 */
     public function import() {
         \WP_CLI::line( 'Importing!' );
+
+        $api = \OpenBreweries\ApiClient::getInstance();
+        $breweries = $api->getIterator();
+
+        $fieldSuffix = "wbl_b_";
+
+        while($breweries->valid()) {
+            $brewery = $breweries->current();
+
+            $slug = is_numeric($brewery['id']) ? sanitize_title($brewery['name']) : $brewery['id'];
+
+            $postData = [
+                'post_title' => $brewery['name'],
+                'post_status' => 'publish',
+                'post_type' => 'brewery',
+                'post_name' => $slug,
+            ];
+
+            if( post_exists($postData['post_title']) ) {
+                \WP_CLI::line( 'Post already exists: ' . $postData['post_title'] );
+            } else {
+                $postId = wp_insert_post($postData);
+
+                if( $postId ) {
+                    \WP_CLI::line( 'Created post: ' . $postData['post_title'] );
+
+                    foreach($brewery as $key => $value) {
+                        update_field($fieldSuffix . $key, $value, $postId);
+                    }
+                } else {
+                    \WP_CLI::line( 'Failed to create post: ' . $postData['post_title'] );
+                }
+            }
+
+            $breweries->next();
+            
+        }
+
+
     }
 
 }
